1f5cf57530c8e9ca90d9678e4102d5d5724943e5
Jonas Buntinx <buntinxjonas@gmail.com>
2020-04-11 19:00:11 +0900

    Version update, added Profile page

--- app/Components/Navigation.purs
+++ app/Components/Navigation.purs
@@ -15,41 +15,54 @@ mkNavigation =
     R.nav
       { className: "text-gray-800 w-full flex items-center justify-between py-3 px-4"
       , children:
-        [ R.div
-            { className: "font-extrabold text-xl"
-            , children:
-              [ R.text "Gatsby.js with Purescript Example" ]
-            }
-        , R.ul
-            { className: "flex-grow flex justify-end items-center"
-            , children:
-              [ R.li
-                  { children:
-                    [ G.link
-                        { to: "/"
-                        , children:
-                          [ R.a
-                              { className: "inline-block py-2 px-4 font-bold"
-                              , children: [ R.text "Home" ]
+          [ R.div
+              { className: "font-extrabold text-xl"
+              , children:
+                  [ R.text "Gatsby.js with Purescript Example" ]
+              }
+          , R.ul
+              { className: "flex-grow flex justify-end items-center"
+              , children:
+                  [ R.li
+                      { children:
+                          [ G.link
+                              { to: "/"
+                              , children:
+                                  [ R.a
+                                      { className: "inline-block py-2 px-4 font-bold"
+                                      , children: [ R.text "Home" ]
+                                      }
+                                  ]
                               }
                           ]
-                        }
-                    ]
-                  }
-              , R.li
-                  { children:
-                    [ G.link
-                        { to: "/about"
-                        , children:
-                          [ R.a
-                              { className: "inline-block py-2 px-4 font-bold"
-                              , children: [ R.text "About" ]
+                      }
+                  , R.li
+                      { children:
+                          [ G.link
+                              { to: "/about"
+                              , children:
+                                  [ R.a
+                                      { className: "inline-block py-2 px-4 font-bold"
+                                      , children: [ R.text "About" ]
+                                      }
+                                  ]
                               }
                           ]
-                        }
-                    ]
-                  }
-              ]
-            }
-        ]
+                      }
+                  , R.li
+                      { children:
+                          [ G.link
+                              { to: "/profile"
+                              , children:
+                                  [ R.a
+                                      { className: "inline-block py-2 px-4 font-bold"
+                                      , children: [ R.text "Profile" ]
+                                      }
+                                  ]
+                              }
+                          ]
+                      }
+                  ]
+              }
+          ]
       }
---
+++ src/pages/profile.js
@@ -0,0 +1,1 @@
+module.exports = require("../../app/Pages/Profile.purs").mkProfile();
